cbaea09c0356ef9511f624073ccb303315032595
Paul E. McKenney <paulmck@kernel.org>
2023-12-04 11:59:18 -0800

    Add and use a DisplayRunLitmus directive
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- ootavacuous.sh
+++ ootavacuous.sh
@@ -15,26 +15,46 @@
 T="`mktemp -d ${TMPDIR-/tmp}/ootavacuous.sh.XXXXXX`"
 trap 'rm -rf $T' 0
 
-awk -v q="'" -v bs='\\' < ootavacuous.tex '
+awk -v q="'" -v dq='"' -v bs='\\' < ootavacuous.tex '
 BEGIN {
-	print "cat << '___EOF___'";
+	print "cat << " sq "___EOF___" sq;
 	print "% Automatically built, do not edit";
 	print "%";
 }
 
 /^@@ DisplayLitmus .* @@$/ {
 	path = $3;
-	print "'___EOF___'";
+	print "___EOF___";
 	print "grep -v " q "^[( ]" bs "*" q " < " path " | c2latex.sh";
-	print "cat << '___EOF___'";
+	print "cat << " sq "___EOF___" sq;
+	next;
+}
+
+/^@@ DisplayRunLitmus .* @@$/ {
+	path = $3;
+	print "{";
+	print bs "scriptsize";
+	print bs "begin{verbatim}";
+	print "___EOF___";
+	print "grep -v " q "^[( ]" bs "*" q " < " path " | c2latex.sh";
+	print "cat << " sq "___EOF___" sq;
+	print bs "end{verbatim}";
+	print "";
+	print "Analysis by " dq bs "co{herd7 -c11 " path dq "}:";
+	print bs "begin{verbatim}";
+	print "___EOF___";
+	print "herd7 -c11 " path " | grep -v " sq "^$" sq " | c2latex.sh ";
+	print "cat << " sq "___EOF___" sq;
+	print bs "end{verbatim}";
+	print "}";
 	next;
 }
 
 /^@@ RunLitmus .* @@$/ {
 	path = $3;
-	print "'___EOF___'";
+	print sq "___EOF___" sq;
 	print "herd7 -c11 " path " | c2latex.sh ";
-	print "cat << '___EOF___'";
+	print "cat << ___EOF___";
 	next;
 }
 
