963c9a73564f453db978b16ae8d45763a8d37fde
Paul E. McKenney <paulmck@kernel.org>
2024-01-07 11:03:43 -0800

    January 7 self-review
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- ootavacuous.sh
+++ ootavacuous.sh
@@ -30,7 +30,7 @@ BEGIN {
 	print bs "scriptsize";
 	print bs "begin{verbatim}";
 	print "___EOF___";
-	print "grep -v " sq "^[( ]" bs "*" sq " < " path " | c2latex.sh";
+	print "grep -v " sq "^[(/ ]" bs "*" sq " < " path " | c2latex.sh";
 	print "cat << " sq "___EOF___" sq;
 	print bs "end{verbatim}";
 	print "}";
@@ -43,7 +43,7 @@ BEGIN {
 	print bs "scriptsize";
 	print bs "begin{verbatim}";
 	print "___EOF___";
-	print "grep -v " sq "^[( ]" bs "*" sq " < " path " | c2latex.sh";
+	print "grep -v " sq "^[(/ ]" bs "*" sq " < " path " | c2latex.sh";
 	print "cat << " sq "___EOF___" sq;
 	print bs "end{verbatim}";
 	print "";
